fec508958080601bc30b9523d7ac39700f2717c6
Abdul Saboor <abdulsaboora691@gmail.com>
2026-06-22 15:10:33 +0500

    upd about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -121,20 +121,6 @@ export default function AboutPage() {
                   className="object-cover" 
                 />
               </motion.div>
-              <motion.div 
-                initial={{ opacity: 0, y: 50 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                viewport={{ once: true, margin: "-20%" }}
-                transition={{ duration: 1, ease: [0.16, 1, 0.3, 1] }}
-                className="relative aspect-square w-full md:w-3/4 ml-auto overflow-hidden bg-gray-900"
-              >
-                <Image 
-                  src="/about.webp" 
-                  alt="Architecture Planning" 
-                  fill 
-                  className="object-cover" 
-                />
-              </motion.div>
             </div>
 
           </div>
